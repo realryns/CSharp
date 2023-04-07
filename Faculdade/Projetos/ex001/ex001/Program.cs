034bd89f9264484eccd08059ef12ea60b46f997d
realryns <rafaelnakajune@hotmail.com>
2023-04-07 11:38:23 -0300

    commit

---
+++ Faculdade/Projetos/ex001/ex001/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+
+namespace ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            int cont = 1;
+
+            while (cont <= 10)
+            {
+                int num = int.Parse(Console.ReadLine());
+                soma = soma + num;
+                cont++;
+            }
+
+            
+
+            Console.WriteLine("oi");
+)
+        }
+    }
+}
