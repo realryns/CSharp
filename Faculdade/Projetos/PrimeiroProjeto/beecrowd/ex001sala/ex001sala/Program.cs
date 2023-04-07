57f4c4382cf4f1f5f5bff7d706ab876a2d68fac8
realryns <rafaelnakajune@hotmail.com>
2023-04-07 11:39:36 -0300

    commit

---
+++ Faculdade/Projetos/PrimeiroProjeto/beecrowd/ex001sala/ex001sala/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+
+int cont = 0;
+int soma = 0;
+while (cont <= 10)
+{
+    int num = int.Parse(Console.ReadLine());
+    cont++;
+    soma = soma + num;
+}
+
+double media = soma / cont;
+Console.WriteLine(media);
