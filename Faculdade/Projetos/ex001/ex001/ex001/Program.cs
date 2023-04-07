eda6a0b6eb4a204263862a838aa445e527ab4e7a
realryns <rafaelnakajune@hotmail.com>
2023-04-07 11:49:51 -0300

    commit

---
+++ Faculdade/Projetos/ex001/ex001/ex001/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double soma = 0;
+double cont = 1;
+
+while (cont <= 10)
+{
+    double num = int.Parse(Console.ReadLine());
+    soma = soma + num;
+    cont++;
+}
+
+double media = soma / (cont - 1);
+
+Console.WriteLine(media);
