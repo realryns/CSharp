3ee763440c5db4ff9ce7daa1852beda19404a5c6
realryns <rafaelnakajune@hotmail.com>
2023-04-26 16:44:41 -0300

    commit

---
+++ Lógica/dardo/dardo/Program.cs
@@ -0,0 +1,43 @@
+﻿/* 
+No arremesso de dardo, o atleta tem três chances para lançar o dardo à maior distância que conseguir. 
+Você deve criar um programa para, dadas as medidas das três tentativas de lançamento, informar qual 
+foi a maior. 
+*/
+
+using System;
+using System.Globalization;
+
+namespace dardo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double A, B, C, maior;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Digite as três distâncias: ");
+            A = Double.Parse(Console.ReadLine(), CI);
+            B = Double.Parse(Console.ReadLine(), CI);
+            C = Double.Parse(Console.ReadLine(), CI);
+
+            if ((A > B) && (A > C))
+            {
+                maior = A;        
+            }
+            else if (B > C)
+            {
+                maior = B;
+            }
+            else
+            {
+                maior = C;
+            }
+
+            Console.WriteLine("MAIOR DISTÂNCIA: {0}", maior.ToString("F2", CI));
+
+        }
+    }
+}
---
+++ Lógica/temperatura/temperatura/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Você irá digitar a temperatura em qual escala? C/F ");
+        }
+    }
+}
