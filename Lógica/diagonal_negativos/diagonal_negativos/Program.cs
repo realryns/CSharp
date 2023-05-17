794052170d260416ac5053fb28ee814cd9b25694
realryns <rafaelnakajune@hotmail.com>
2023-05-17 13:53:35 -0300

    commit

---
+++ Lógica/diagonal_negativos/diagonal_negativos/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Fazer um programa para ler um número inteiro N (máximo = 10) e uma matriz quadrada de ordem N 
+contendo números inteiros. Em seguida, mostrar a diagonal principal e a quantidade de valores 
+negativos da matriz. */
+
+using System;
+
+namespace diagonal_negativos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int negativos = 0, diagonal;
+
+            Console.Write("Qual a ordem da matriz? ");
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[N, N];
+            
+            for ( int i = 0; i < N; i++ )
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("Elemento [{0}, {1}]: ", i, j);
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                    if (matriz[i, j] < 0 )
+                    {
+                        negativos++;
+                    }
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL: ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(matriz[i, i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("QUANTIDADE DE NEGATIVOS: {0}", negativos);
+            
+
+        }
+    }
+}
---
+++ Lógica/soma_linhas/soma_linhas/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Fazer um programa para ler dois números inteiros M e N (máximo = 10). Em seguida, ler uma matriz 
+de M linhas e N colunas contendo números reais. Gerar um vetor de modo que cada elemento do vetor 
+seja a soma dos elementos da linha correspondente da matriz. Mostrar o vetor gerado. */
+
+using System;
+using System.Globalization;
+
+namespace soma_linhas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Qual a quantidade de linhas da matriz? ");
+            int M = int.Parse(Console.ReadLine());
+            Console.Write("Qual a quantidade de colunas da matriz? ");
+            int N = int.Parse(Console.ReadLine());
+
+            double[,] matriz = new double[M, N];
+            double[] soma = new double[M];
+
+            for (int i = 0; i < M; i++)
+            {
+                Console.WriteLine("Digite os elementos da {0}a linha: ", i + 1);
+                for (int j = 0; j < N; j++)
+                {
+                    matriz[i, j] = Double.Parse(Console.ReadLine(), CI);
+                    soma[i] += matriz[i, j];
+                }
+            }
+
+            Console.WriteLine("VETOR GERADO: ");
+            for (int i = 0; i < M; i++)
+            {
+                Console.WriteLine(soma[i].ToString("F1", CI));
+            }
+
+        }
+    }
+}
