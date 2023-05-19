261f0f8e90ae7862fc9ce437db839cd9c38c52cf
realryns <rafaelnakajune@hotmail.com>
2023-05-19 15:54:43 -0300

    commit

--- Beecrowd/C#/beecrowd_1180/beecrowd_1180/Program.cs
+++ Beecrowd/C#/beecrowd_1180/beecrowd_1180/Program.cs
@@ -1,20 +1,27 @@
-﻿int N = int.Parse(Console.ReadLine());
-
-int[] X = new int[N];
-int menor = 0, pos = 0;
-
-for (int i = 0; i < X.Length; i++)
+﻿using System;
+internal class Program
 {
-    X[i] = Console.Read();
-    if (X[i] < menor)
+    private static void Main(string[] args)
     {
-        menor = X[i];
-        pos = i;
+        
+        int N = int.Parse(Console.ReadLine());
+        int[] X = new int[N];
+        int menor = 1000, pos = 0;
+
+        string[] texto = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < N; i++)
+        {
+            X[i] = int.Parse(texto[i]);
+            if (X[i] < menor)
+            {
+                menor = X[i];
+                pos = i;
+            }
+        }
+
+        Console.WriteLine("Menor valor: {0}", menor);
+        Console.WriteLine("Posicao: {0}", pos);
+
     }
 }
-
-Console.WriteLine("Menor valor: {0} ", menor);
-Console.WriteLine("Posicao: {0} ", pos);
-
-
-
